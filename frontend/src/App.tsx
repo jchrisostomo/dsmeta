b98ec304857b0b2fb57c6f4d664c6914df01268a
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-09-08 03:12:17 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo!</h1>
+      <Header />
       <NotificationButton />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <NotificationButton />
+          <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
