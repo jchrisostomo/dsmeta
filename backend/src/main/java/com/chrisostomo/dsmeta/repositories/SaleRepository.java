87db93040a44b81cfd31ba805c0c0d2eaefb536e
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-09-10 13:19:17 -0300

    API test

---
+++ backend/src/main/java/com/chrisostomo/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.chrisostomo.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chrisostomo.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ backend/src/main/java/com/chrisostomo/dsmeta/services/SaleService.java
@@ -0,0 +1,21 @@
+package com.chrisostomo.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.chrisostomo.dsmeta.entities.Sale;
+import com.chrisostomo.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();		
+	}
+
+}
